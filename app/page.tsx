000fb20cf96d3263a0ec92e328cb2d254fc42bf4
Zhurvlad <murmanskvmf@mail.ru>
2024-07-29 02:22:58 +0300

    Product card

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import {
   Categories,
   Container,
+  ProductsGroupList,
   SortPopup,
   Title,
   TopBar,
 } from "@/components/shared";
 import { Filters } from "@/components/shared/filters";
+import { ProductCard } from "@/components/shared/product-card";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
@@ -17,14 +19,139 @@ export default function Home() {
       </Container>
       <TopBar />
       <Container className=" mt-10  pb-14">
-        <div className="flex gap-[60px]">
+        <div className="flex gap-[80px]">
           {/*Фильтрация */}
           <div className="w-[250px]">
             <Filters />
           </div>
           {/*Список товаров */}
           <div className="flex-1">
-            <div className="flex flex-col gap-16">Список пицц</div>
+            <div className="flex flex-col gap-16">
+              <ProductsGroupList
+                title="Пиццы"
+                categoryId={1}
+                items={[
+                  {
+                    id: 1,
+                    name: "Пицца",
+                    imageUrl:
+                      "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 2,
+                    name: "Пицца",
+                    imageUrl:
+                      "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 3,
+                    name: "Пицца",
+                    imageUrl:
+                      "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 4,
+                    name: "Пицца",
+                    imageUrl:
+                      "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 5,
+                    name: "Пицца",
+                    imageUrl:
+                      "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 6,
+                    name: "Пицца",
+                    imageUrl:
+                      "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 7,
+                    name: "Пицца",
+                    imageUrl:
+                      "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                ]}
+              />
+              <ProductsGroupList
+                title="Комбо"
+                categoryId={2}
+                items={[
+                  {
+                    id: 1,
+                    name: "Пицца",
+                    imageUrl:
+                      "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 2,
+                    name: "Пицца",
+                    imageUrl:
+                      "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 3,
+                    name: "Пицца",
+                    imageUrl:
+                      "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 4,
+                    name: "Пицца",
+                    imageUrl:
+                      "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 5,
+                    name: "Пицца",
+                    imageUrl:
+                      "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 6,
+                    name: "Пицца",
+                    imageUrl:
+                      "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 7,
+                    name: "Пицца",
+                    imageUrl:
+                      "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg",
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                ]}
+              />
+            </div>
           </div>
         </div>
       </Container>
--- components/shared/categories.tsx
+++ components/shared/categories.tsx
@@ -1,37 +1,39 @@
+"use client";
 import React from "react";
 import { cn } from "@/lib/utils";
+import { useCategoryStore } from "../../../store/category";
 
 interface Props {
   className?: string;
 }
 
 const pizzasCategory = [
-  "Пиццы",
-  "Комбо",
-  "Закуски",
-  "Коктейли",
-  "Кофе",
-  "Напитки",
-  "Десерты",
+  { id: 1, title: "Пиццы" },
+  { id: 2, title: "Комбо" },
+  { id: 3, title: "Закуски" },
+  { id: 4, title: "Коктейли" },
+  { id: 5, title: "Кофе" },
+  { id: 6, title: "Напитки" },
+  { id: 7, title: "Десерты" },
 ];
 
-const activeIndex = 0;
-
 export const Categories: React.FC<Props> = ({ className }) => {
+  const categoryActiveId = useCategoryStore((state) => state.activeId);
   return (
     <div
       className={cn("inline-flex gap-1 bg-gray-50 p-1 rounded-2xl", className)}
     >
-      {pizzasCategory.map((item, index) => (
+      {pizzasCategory.map(({ title, id }, index) => (
         <a
-          key={item}
+          href={`/#${title}`}
+          key={id}
           className={cn(
             "flex items-center font-bold h-11 rounded-2xl px-5",
-            activeIndex === index &&
+            categoryActiveId === id &&
               "bg-white shadow-md shadow-gray-200 text-primary"
           )}
         >
-          <button>{item}</button>
+          <button>{title}</button>
         </a>
       ))}
     </div>
